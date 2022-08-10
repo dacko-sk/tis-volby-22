2500574175d6b7631459065ef7801ecb8f444816
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2022-08-10 13:38:24 +0200

    add social media icons & facebook feed

---
+++ src/components/FbFeed.jsx
@@ -0,0 +1,29 @@
+import { memo, useEffect } from 'react';
+
+const FbFeed = memo(({ appId, name, url }) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src = 'https://connect.facebook.net/sk_SK/sdk.js#xfbml=1&version=v14.0&appId=' + appId + '&autoLogAppEvents=1';
+    script.async = true;
+    script.defer = true;
+    script.crossOrigin = 'anonymous';
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+
+  return (
+    <div className="fb-feed">
+        <div id="fb-root"></div>
+        <div className="fb-page" data-href={ url } data-tabs="timeline" data-width="" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+            <blockquote cite={ url } className="fb-xfbml-parse-ignore"><a href={ url }>{ name }</a></blockquote>
+        </div>
+    </div>
+  );
+});
+
+export default FbFeed;
--- src/components/structure/Footer.jsx
+++ src/components/structure/Footer.jsx
@@ -1,7 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { SocialIcon } from 'react-social-icons';
+import { colorOrange } from '../../api/constants';
 import { imgPath } from '../../api/helpers';
+import FbFeed from '../FbFeed';
 
 function Footer() {
   return (
@@ -28,11 +32,19 @@ function Footer() {
               </p>
             </Col>
             <Col md={6} lg={4}>
+            <h2>Newsletter</h2>
+              <Button className="my-3" href="https://eepurl.com/doWD8X" target="_blank" variant="secondary">Prihlásiť sa na newsletter</Button>
               <h2>Sledujte nás</h2>
+              <div className="social-icons my-3">
+                <SocialIcon bgColor={ colorOrange } className="me-2" url="https://www.facebook.com/transparencysk" />
+                <SocialIcon bgColor={ colorOrange } className="me-2" url="https://twitter.com/transparencysk" />
+                <SocialIcon bgColor={ colorOrange } className="me-2" url="https://www.linkedin.com/company/transparency-international-slovakia" />
+                <SocialIcon bgColor={ colorOrange } url="https://www.youtube.com/user/TISlovensko" />
+              </div>
             </Col>
-            {/* <Col md={12} lg={4}>
-              <h2>Facebook</h2>
-            </Col> */}
+            <Col md={12} lg={4}>
+              <FbFeed appId="210544879524339" name="Transparency International Slovensko" url="https://www.facebook.com/transparencysk/" />
+            </Col>
           </Row>
         </Container>
       </div>
