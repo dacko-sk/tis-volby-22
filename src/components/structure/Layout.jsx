95efdeaac2c17f1af5c22e87db312a49f0f5eb62
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2022-08-13 00:16:16 +0200

    add google analyticks

--- src/components/structure/Layout.jsx
+++ src/components/structure/Layout.jsx
@@ -1,6 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { usePapaParse } from 'react-papaparse';
+import ReactGA from 'react-ga';
+import { gaTrackingId } from '../../api/constants';
 import useData, { csvFile, buildParserConfig } from '../../context/DataContext';
 import Header from './Header';
 import Footer from './Footer';
@@ -9,6 +11,7 @@ function Layout() {
     const { setCsvData } = useData();
     const { readRemoteFile } = usePapaParse();
     const { pathname } = useLocation();
+    const [gaReady, setGaReady] = useState(false);
 
     useEffect(() => {
         // load election data from CSV API and store in context provider
@@ -17,6 +20,20 @@ function Layout() {
         const parserConfig = buildParserConfig(setCsvData);
         readRemoteFile(csvFile, parserConfig);
     }, []);
+
+    useEffect(() => {
+        // init GA
+        if (!window.location.href.includes('localhost')) {
+            ReactGA.initialize(gaTrackingId);
+            setGaReady(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (gaReady) {
+            ReactGA.pageview(pathname);
+        }
+    }, [gaReady, pathname]);
 
     useEffect(() => {
         // scroll to top when route changes
