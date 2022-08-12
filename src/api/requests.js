fda788f6a2f15a4cdd91b351332294eafcf10ab7
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2022-08-12 15:39:47 +0200

    remove unused file

--- src/api/requests.js
+++
@@ -1,4 +0,0 @@
-export const getAggregatedCsv = () => {
-    return fetch('https://raw.githubusercontent.com/matusv/transparent-account-data-slovak-elections-2022/main/aggregation.csv')
-        .then(res => res.text())
-};
