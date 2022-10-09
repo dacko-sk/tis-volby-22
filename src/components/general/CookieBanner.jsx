2a81ca21c09a893f167913cae5820d286632f9e2
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2022-10-09 17:00:10 +0200

    do not hide cookie settings button

--- src/components/general/CookieBanner.jsx
+++ src/components/general/CookieBanner.jsx
@@ -53,96 +53,91 @@ function CookieBanner() {
         setAnaliticsStorage(false);
     }, []);
 
-    if (cookies && has(cookies, 'open') && !cookies.open) {
-        return (
-            <Button
-                className="d-block my-3"
-                variant="primary"
-                onClick={openSettingsClick}
-            >
+    return (
+        <div className="my-3">
+            <Button variant="primary" onClick={openSettingsClick}>
                 Nastavenia cookies
             </Button>
-        );
-    }
+            {cookies.open && (
+                <Container fluid className="cookie-banner py-4">
+                    <Row className="justify-content-center">
+                        <Col xs="auto">
+                            <p>
+                                Táto webová stránka používa cookies, aby vám
+                                priniesla čo najlepší online zážitok.{' '}
+                                <a
+                                    href="https://transparency.sk/sk/sukromie/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    Zistiť viac
+                                </a>
+                            </p>
 
-    return (
-        <Container fluid className="cookie-banner py-4">
-            <Row className="justify-content-center">
-                <Col xs="auto">
-                    <p>
-                        Táto webová stránka používa cookies, aby vám priniesla
-                        čo najlepší online zážitok.{' '}
-                        <a
-                            href="https://transparency.sk/sk/sukromie/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            Zistiť viac
-                        </a>
-                    </p>
+                            <Accordion flush className="my-3">
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>
+                                        Voliteľné cookies
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                        <Form className="d-flex justify-content-center flex-column flex-sm-row">
+                                            <Form.Check
+                                                className="mx-2"
+                                                disabled
+                                                checked
+                                                type="switch"
+                                                id="cookies-necessary"
+                                                label="Nevyhnutné cookies"
+                                            />
+                                            <Form.Check
+                                                className="mx-2"
+                                                checked={checkFunctional}
+                                                onChange={checkFunctionalChange}
+                                                type="switch"
+                                                label="Funkčné cookies"
+                                                id="cookies-functional"
+                                            />
+                                            <Form.Check
+                                                className="mx-2"
+                                                checked={checkAnalytics}
+                                                onChange={checkAnalyticsChange}
+                                                type="switch"
+                                                label="Analytické cookies"
+                                                id="cookies-analytics"
+                                            />
+                                        </Form>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
 
-                    <Accordion flush className="my-3">
-                        <Accordion.Item eventKey="0">
-                            <Accordion.Header>
-                                Voliteľné cookies
-                            </Accordion.Header>
-                            <Accordion.Body>
-                                <Form className="d-flex justify-content-center flex-column flex-sm-row">
-                                    <Form.Check
-                                        className="mx-2"
-                                        disabled
-                                        checked
-                                        type="switch"
-                                        id="cookies-necessary"
-                                        label="Nevyhnutné cookies"
-                                    />
-                                    <Form.Check
-                                        className="mx-2"
-                                        checked={checkFunctional}
-                                        onChange={checkFunctionalChange}
-                                        type="switch"
-                                        label="Funkčné cookies"
-                                        id="cookies-functional"
-                                    />
-                                    <Form.Check
-                                        className="mx-2"
-                                        checked={checkAnalytics}
-                                        onChange={checkAnalyticsChange}
-                                        type="switch"
-                                        label="Analytické cookies"
-                                        id="cookies-analytics"
-                                    />
-                                </Form>
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-
-                    <div className="d-flex justify-content-center flex-column flex-sm-row">
-                        <Button
-                            className="mb-2 mb-sm-0 me-0 me-sm-3"
-                            variant="success"
-                            onClick={rejectAllClick}
-                        >
-                            Odmientnuť všetky
-                        </Button>
-                        <Button
-                            className="mb-2 mb-sm-0 me-0 me-sm-3"
-                            variant="success"
-                            onClick={acceptSelectionClick}
-                        >
-                            Potvrdiť výber
-                        </Button>
-                        <Button
-                            // className="m-2"
-                            variant="secondary"
-                            onClick={acceptAllClick}
-                        >
-                            Prijať všetky
-                        </Button>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+                            <div className="d-flex justify-content-center flex-column flex-sm-row">
+                                <Button
+                                    className="mb-2 mb-sm-0 me-0 me-sm-3"
+                                    variant="success"
+                                    onClick={rejectAllClick}
+                                >
+                                    Odmientnuť všetky
+                                </Button>
+                                <Button
+                                    className="mb-2 mb-sm-0 me-0 me-sm-3"
+                                    variant="success"
+                                    onClick={acceptSelectionClick}
+                                >
+                                    Potvrdiť výber
+                                </Button>
+                                <Button
+                                    // className="m-2"
+                                    variant="secondary"
+                                    onClick={acceptAllClick}
+                                >
+                                    Prijať všetky
+                                </Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            )}
+        </div>
     );
 }
 
